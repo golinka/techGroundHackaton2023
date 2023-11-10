6b7a29305a60d69c1b8dc777f150538d93b29c0a
Artem Golinka <artem.golinka@jiji.ng>
2023-11-10 17:40:02 +0100

    feat(app): added bootstrap

--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="admin-page">
-    Admin page
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <h1>Hello, Bootstrap and Vite!</h1>
+          <button class="btn btn-primary">Primary button</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
